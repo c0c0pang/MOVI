b59a28f27a911dac35d84ef7d104a95e2b051995
c0c0pang <coc1789@naver.com>
2022-11-12 17:17:06 +0900

    feat: tab bar 구현

--- lib/communication/Community.dart
+++ lib/communication/Community.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class COMMUNITY extends StatefulWidget {
   const COMMUNITY({super.key});
 
@@ -6,38 +7,31 @@ class COMMUNITY extends StatefulWidget {
   State<COMMUNITY> createState() => mainCommunityPage();
 }
 
-class mainCommunityPage extends State<COMMUNITY>{
+class mainCommunityPage extends State<COMMUNITY> {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('커뮤니티',style: TextStyle(color: Colors.black,fontSize: 25)),
-        centerTitle: false,
-        elevation: 0,
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          InkWell(
-            onTap: (){},
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black,
-                    ),
-                  )
-              ),
-              child: Text('영화평가 & 추천',style: TextStyle(fontSize: 23)),
-            ),
+  Widget build(BuildContext context) {
+    return MaterialApp(home: DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+              '커뮤니티', style: TextStyle(color: Colors.black, fontSize: 25)),
+          centerTitle: false,
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text('영화평가 & 추천',
+                  style: TextStyle(color: Colors.black, fontSize: 18))),
+              Tab(child: Text(
+                  '자유게시판', style: TextStyle(color: Colors.black, fontSize: 18)))
+            ], indicatorColor: Colors.black,
           ),
-
-          Container(
-            child: Text('자유게시판',style: TextStyle(fontSize: 23)),
-          ),
-        ],
+        ),
+        body: TabBarView(
+          children: [
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
