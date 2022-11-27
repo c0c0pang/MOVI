466433bbcdf9ed161e9632b3e3f2b6c7a0b8a3e9
shin5774 <babo5774@naver.com>
2022-11-28 01:10:14 +0900

    feat: 매칭 리스트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:moviproject/message_list_screen.dart';
 import './communication/Community.dart';
 import './matching/Match.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'dart:async';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+  //Firebase.initializeApp(
+  //);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MATCH(),
+      home: const MATCH(),
     );
   }
 }
--- lib/matching/Match.dart
+++ lib/matching/Match.dart
@@ -7,39 +7,6 @@ class MATCH extends StatefulWidget {
   @override
   State<MATCH> createState() => mainMatchPage();
 }
-/*
-class mainMatchPage extends State<MATCH> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-              '커뮤니티', style: TextStyle(color: Colors.black, fontSize: 25)),
-          centerTitle: false,
-          bottom: TabBar(
-            tabs: [
-              Tab(child: Text('장기모임',
-                  style: TextStyle(color: Colors.black, fontSize: 18))),
-              Tab(child: Text(
-                  '단기모임', style: TextStyle(color: Colors.black, fontSize: 18)))
-            ], indicatorColor: Colors.black,
-          ),
-
-        ),
-        body: TabBarView(
-          children: [
-            MatchingLongTerm(),
-            MatchingShortTerm(),
-          ],
-        ),
-      ),
-    ));
-  }
-}
- */
 
 class mainMatchPage extends State<MATCH> with TickerProviderStateMixin {
   late TabController _tc;
@@ -86,7 +53,154 @@ class mainMatchPage extends State<MATCH> with TickerProviderStateMixin {
               ))
         ],
       ),
-    )
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context)=> addMatchPage()),
+          );
+
+        },
+        child: Icon(Icons.add_circle),
+      ),
+    ),
+    );
+  }
+}
+
+class addMatchPage extends StatelessWidget{
+  final areaTextController= TextEditingController();
+  final timeTextController= TextEditingController();
+  final dateTextController= TextEditingController();
+  final peopleTextController= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 500,
+        height: 500,
+
+        padding: EdgeInsets.all(50),
+        margin: EdgeInsets.all(50),
+
+        decoration: BoxDecoration(
+          color:Colors.black45,
+
+          borderRadius: BorderRadius.all(
+            Radius.circular(70),
+          )
+        ),
+        child:Expanded(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[
+                  Row(
+                    children:<Widget> [
+                      SizedBox(width: 30,),
+                      new Text(
+                        "지역",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontFamily: 'jua', fontSize: 20,color: Colors.white),
+                      ),
+                      SizedBox(width: 30,),
+                      Container(
+                        child: new Flexible(
+                          child: new TextField(
+                            controller: areaTextController,
+                          ),
+                        ),
+                        width: 100, //TextField 크기
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children:<Widget> [
+                      SizedBox(width: 30,),
+                      new Text(
+                        "시간",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontFamily: 'jua', fontSize: 20,color: Colors.white),
+                      ),
+                      SizedBox(width: 30,),
+                      Container(
+                        child: new Flexible(
+                          child: new TextField(
+                            controller: timeTextController,
+                          ),
+                        ),
+                        width: 100, //TextField 크기
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children:<Widget> [
+                      SizedBox(width: 30,),
+                      new Text(
+                        "날짜",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontFamily: 'jua', fontSize: 20,color: Colors.white),
+                      ),
+                      SizedBox(width: 30,),
+                      Container(
+                        child: new Flexible(
+                          child: new TextField(
+                            controller: dateTextController,
+                          ),
+                        ),
+                        width: 100, //TextField 크기
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children:<Widget> [
+                      SizedBox(width: 30,),
+                      new Text(
+                        "인원",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontFamily: 'jua', fontSize: 20,color: Colors.white),
+                      ),
+                      SizedBox(width: 30,),
+                      Container(
+                        child: new Flexible(
+                          child: new TextField(
+                            controller: peopleTextController,
+                          ),
+                        ),
+                        width: 100, //TextField 크기
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child:Container(
+                      color: Colors.white54,
+                      child:OutlinedButton(
+                        onPressed: (){},
+                        child: Text(
+                          "게시물 올리기",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    )
+                  )
+                ],
+              )
+            ],
+          )
+        )
+
+      ),
     );
   }
 }
